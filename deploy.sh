1399a55c06d7d35ce89b9a920141324bd3d062db
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-12-20 16:01:20 -0700

    bugfix: typo

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ fi
 
 # Replace placeholders in pve-secret.yml
 sed -i -e "s#{TOKEN_NAME}#s$(echo -n $TOKEN_NAME | base64)#g" ./manifests/pve-secret.yaml
-sed -i -e "s#{TOKEN_VALUE}#$(echo -n $TOKEN_VALUE | base64)#g" ./manifests/pve-secret.yml
+sed -i -e "s#{TOKEN_VALUE}#$(echo -n $TOKEN_VALUE | base64)#g" ./manifests/pve-secret.yaml
 
 # Apply the Kustomize configuration
 kubectl apply -k manifests/
