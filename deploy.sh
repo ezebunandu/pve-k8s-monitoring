583710cf5d037d51bd52ccfb64bbb8f45c34af0d
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-12-20 15:50:21 -0700

    secrets: reference correct file in deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ if [ -z "$TOKEN_NAME" ] || [ -z "$TOKEN_VALUE" ]; then
 fi
 
 # Replace placeholders in pve-secret.yml
-sed -i '' "s/\${TOKEN_NAME}/$(echo -n $TOKEN_NAME | base64)/" manifests/pve-secret.yml
-sed -i '' "s/\${TOKEN_VALUE}/$(echo -n $TOKEN_VALUE | base64)/" manifests/pve-secret.yml
+sed -i '' "s/\${TOKEN_NAME}/$(echo -n $TOKEN_NAME | base64)/" manifests/pve-secret.yaml
+sed -i '' "s/\${TOKEN_VALUE}/$(echo -n $TOKEN_VALUE | base64)/" manifests/pve-secret.yaml
 
 # Apply the Kustomize configuration
 kubectl apply -k manifests/
